20f3b0284e298a8979a7993cec5106c79799a6c0
甜豆智能 <@DESKTOP-3JE31RI>
2017-11-28 17:08:42 +0800

    用户登录注册 controller

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -63,7 +63,18 @@ public class UserController {
 	}
 	
 	
-	
+	@RequestMapping(value="get_user_info",method=RequestMethod.GET)
+	@ResponseBody
+	public ServerResponse<User> getUserInfo(HttpSession session){
+		
+		User user = (User) session.getAttribute(Const.CURRENT_USER);
+		if(user!=null) {
+			return ServerResponse.createBySuccess(user);
+		}
+		
+		return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息");
+		
+	}
 	
 	
 	
